18eccf3d8cbebfa8fa848da38aad14fa30aea2c6
Brian Quinlan <b_quinny@hotmail.com>
2018-10-25 16:00:54 +0000

    added add_task.html

--- taskmanager.py
+++ taskmanager.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 from flask_pymongo import PyMongo
 
@@ -13,6 +13,16 @@ mongo = PyMongo(app)
 def show_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks = tasks)
+    
+    
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    if request.method == "POST":
+        mongo.db.tasks.insert_one(request.form.to_dict())
+        return redirect("/")
+    else:
+        categories = mongo.db.categories.find()
+        return render_template("add_task.html", categories=categories)
 
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
